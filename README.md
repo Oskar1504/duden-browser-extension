4e82be5fdabb539c4e8b024b833dd709507bbcc4
Oskar Pester <66569441+Oskar1504@users.noreply.github.com>
2022-09-23 11:41:13 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,13 @@
+# Duden.de Adblocker extension
+Small Extension for Chrome and Opera which removes all adds on duden.de website
+
+ATM you need to download this repo to install it. Working on it to add it to the extension store.
+
+## installation Chrome and Opera
+- download this repo as .zip
+- extract downloaded zip into folder
+- open [chrome://extensions/](chrome://extensions/) in your chrome browse
+- or open settings -> extensions
+- top right enable "Developer mode"
+- top left click "Load unpacked"
+- select extracted folder
---
+++ content.js
@@ -0,0 +1,45 @@
+console.log("Duden Extension loaded")
+
+// clicks the dsgvo popup on page load
+if(document.querySelector(".message-safe-area-holder button")){
+	document.querySelector(".message-safe-area-holder button").click()
+}
+
+let querySelectorStrings = [
+	'div[id*="ad"]',
+	".tile__wrapper",
+	"div[data-google-query-id]"
+]
+
+
+let interval = window.setInterval(removeElements, 1000)
+
+let maxIntervals = 10
+let intervalCount = 0
+
+removeElements()
+
+function removeElements(){
+	console.log("[Duden Extensions] Starting removing Ad elements")
+	let keepInterval = false
+	// removes all ad containers
+	querySelectorStrings.forEach(queryString => {
+		let l = document.querySelectorAll(queryString).length
+		if(l > 0){
+			keepInterval = true
+			document.querySelectorAll(queryString).forEach(elm => {
+				elm.innerHTML = ""
+				elm.removeAttribute('style');
+				elm.removeAttribute('id');
+				elm.removeAttribute('class');
+			})
+			console.log("[Duden Extensions] Removed: " + l + " html elements")
+		}
+	})
+	intervalCount ++
+
+	if(!keepInterval || intervalCount >= maxIntervals){
+		clearInterval(interval)
+		console.log("[Duden Extensions] Remove interval stopped")
+	}
+}
